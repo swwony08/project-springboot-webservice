349811c7aa9fed9a206bc2cd25c343a7663548bb
wony08 <wony08@naver.com>
2020-10-21 21:44:45 +0900

    게시글 코드 커밋 1

---
+++ src/main/java/com/wony/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,48 @@
+package com.wony.book.springboot.service.posts;
+
+import com.wony.book.springboot.domain.posts.Posts;
+import com.wony.book.springboot.domain.posts.PostsRepository;
+import com.wony.book.springboot.web.dto.PostsListResponseDto;
+import com.wony.book.springboot.web.dto.PostsResponseDto;
+import com.wony.book.springboot.web.dto.PostsSaveRequestDto;
+import com.wony.book.springboot.web.dto.PostsUpdateRequestsDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestsDto requestsDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestsDto.getTitle(), requestsDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id) );
+
+        return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/wony/book/springboot/web/IndexController.java
@@ -0,0 +1,25 @@
+package com.wony.book.springboot.web;
+
+import com.wony.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+}
