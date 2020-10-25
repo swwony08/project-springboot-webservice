a4539a2e5d1abc8691c398237b3b44550a3bfa03
wony08 <wony08@naver.com>
2020-10-25 17:27:05 +0900

    구글 로그인 추가

---
+++ src/main/java/com/wony/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,18 @@
+package com.wony.book.springboot.config.auth.dto;
+
+import java.io.Serializable;
+
+import com.wony.book.springboot.domain.user.User;
+import lombok.Getter;
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
