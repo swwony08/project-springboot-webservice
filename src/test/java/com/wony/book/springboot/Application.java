ab6dd13ccb915aa13c36f871403f8a856361c467
wony08 <wony08@naver.com>
2020-10-28 23:46:36 +0900

    기존 테스트에 시큐리티 적용하기

--- src/test/java/com/wony/book/springboot/Application.java
+++ src/test/java/com/wony/book/springboot/Application.java
@@ -1,9 +1,9 @@
 package com.wony.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Configuration;
 
-@EnableJpaAuditing
+@Configuration
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
--- src/test/java/com/wony/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/wony/book/springboot/web/HelloControllerTest.java
@@ -1,8 +1,13 @@
 package com.wony.book.springboot.web;
+import com.wony.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,12 +18,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello_return() throws Exception{
         String hello = "hello";
@@ -29,6 +37,7 @@ public class HelloControllerTest {
 
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto_return() throws Exception{
         String name = "hello";
